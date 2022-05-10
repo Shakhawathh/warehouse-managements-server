05b1de250b063056b31696b3735b3bb38800dd39
Shakhawathh <shakhawathh55@gmail.com>
2022-05-10 11:48:52 +0600

    last

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 require('dotenv').config()
 
 const app = express()
